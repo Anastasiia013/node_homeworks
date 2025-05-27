8cd9a80878ddf4560d708a6136b7f64367e9e1c1
ANASTASIIA <anastasiaposokhova@gmail.com>
2025-05-27 16:15:53 +0200

    add hw1

---
+++ simpleLogger/app.js
@@ -0,0 +1,6 @@
+const { logMessage } = require('./logger');
+
+logMessage('Приложение запущено.');
+logMessage('Выполняется операция A.');
+logMessage('Операция A завершена.');
+logMessage('Приложение завершено.');
